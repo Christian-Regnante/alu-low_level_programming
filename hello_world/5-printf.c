526abc0e0ddad589b4ce7e1da6cb4faad977e2f0
christian-regnante <c.ngwino@alustudent.com>
2026-05-22 13:19:46 +0200

    fixed betty errors on this file

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    puts("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
