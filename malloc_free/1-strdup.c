70fbd8596a717400ea942805f2413fafc7aeea6e
christian-regnante <c.ngwino@alustudent.com>
2026-06-19 19:55:48 +0200

    adjusted based on betty style

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,25 +9,25 @@
  */
 char *_strdup(char *str)
 {
-    char *copy;
-    unsigned int i;
-    unsigned int len;
+char *copy;
+unsigned int i;
+unsigned int len;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    len = 0;
+len = 0;
 
-    while (str[len] != '\0')
-        len++;
+while (str[len] != '\0')
+len++;
 
-    copy = malloc((len + 1) * sizeof(char));
+copy = malloc((len + 1) * sizeof(char));
 
-    if (copy == NULL)
-        return (NULL);
+if (copy == NULL)
+return (NULL);
 
-    for (i = 0; i <= len; i++)
-        copy[i] = str[i];
+for (i = 0; i <= len; i++)
+copy[i] = str[i];
 
-    return (copy);
+return (copy);
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,36 +10,30 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	unsigned int len1 = 0;
-	unsigned int len2 = 0;
-	unsigned int i;
-	unsigned int j;
-
+char *result;
+unsigned int len1 = 0;
+unsigned int len2 = 0;
+unsigned int i;
+unsigned int j;
 	if (s1 != NULL)
 	{
 		while (s1[len1] != '\0')
 			len1++;
 	}
-
 	if (s2 != NULL)
 	{
 		while (s2[len2] != '\0')
 			len2++;
 	}
-
 	result = malloc((len1 + len2 + 1) * sizeof(char));
-
 	if (result == NULL)
 		return (NULL);
-
 	i = 0;
 	while (i < len1)
 	{
 		result[i] = s1[i];
 		i++;
 	}
-
 	j = 0;
 	while (j < len2)
 	{
@@ -47,8 +41,6 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		j++;
 	}
-
 	result[i] = '\0';
-
-	return (result);
+return (result);
 }
